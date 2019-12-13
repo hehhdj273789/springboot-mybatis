17812a3add6932d7c8d60f08dfadbc63ccc0e512
huanglp <2164720006@qq.com>
2019-12-13 11:54:38 +0800

    '这是mybatis+注解方式实现的增删改查'

---
+++ src/main/java/com/sitech/springboot/mapper/DepartmentMapper.java
@@ -0,0 +1,19 @@
+package com.sitech.springboot.mapper;
+
+import com.sitech.springboot.bean.Department;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface DepartmentMapper {
+    @Select("select * from department where id=#{id}")
+    public Department getDeptById(Integer id) ;
+
+    @Delete("delete from department where id=#{id}")
+    public int deleteDeptById(Integer id);
+
+    @Insert("insert into department(departmentName) values(#{departmentName})")
+    public int insertDept(Department department) ;
+
+    @Update("update department set departmentName=#{departmentName} where id=#{id}")
+    public int updateDept(Department department) ;
+}
